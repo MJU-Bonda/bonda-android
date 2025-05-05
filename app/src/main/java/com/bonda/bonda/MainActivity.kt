c1fc952a8816b8e2ed2ee18d2d4cf6743c91aa7e
wooooooooook <otsu9110@naver.com>
2025-05-06 02:06:27 +0900

    chore. debug 버튼 추가

--- app/src/main/java/com/bonda/bonda/MainActivity.kt
+++ app/src/main/java/com/bonda/bonda/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.bonda.bonda
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bonda.bonda.databinding.ActivityMainBinding
+import com.bonda.bonda.ui.article.detail.ArticleDetailActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +26,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.buttonDebug.setOnClickListener {
+            val intent = Intent(this, ArticleDetailActivity::class.java)
+            startActivity(intent)
         }
     }
 }
