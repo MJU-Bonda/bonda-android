ecbcb2149d59643d5fb4775dd68266faa273f9a8
wooooooooook <otsu9110@naver.com>
2025-05-13 02:29:39 +0900

    chore. navigation bar 영역 침범 문제 수정

--- app/src/main/java/com/bonda/bonda/MainActivity.kt
+++ app/src/main/java/com/bonda/bonda/MainActivity.kt
@@ -5,9 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bonda.bonda.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -21,24 +22,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        이 코드는 앱을 시스템 바(status bar, navigation bar)를 포함한 화면 가장자리(edge)까지 콘텐츠가 그려지도록 자동 설정해 줍니다.
         enableEdgeToEdge()
 
-//        이 코드는 윈도우 인셋(window insets) 을 뷰에 적용해서 시스템 바(상태 표시줄·네비게이션 바)에 가려지지 않도록 내부 여백(padding)을 자동으로 조정해 주는 역할을 합니다.
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
 
-        val navView: BottomNavigationView = binding.bottomNav
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_books, R.id.navigation_library, R.id.navigation_profile
-            )
-        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
